8227e78d1e35ebba25b775bbbd680878427aa692
Tim Paine <t.paine154@gmail.com>
2019-12-30 03:23:24 +0000

    fix lint

--- tornado_sqlalchemy_login/__init__.py
+++ tornado_sqlalchemy_login/__init__.py
@@ -1,4 +1,3 @@
 from ._version import __version__  # noqa: F401
-from .application import make_application
+from .application import make_application  # noqa: F401
 from .handlers import ServerHandler, HTMLOpenHandler, LoginHandler, LogoutHandler, RegisterHandler, APIKeyHandler  # noqa: F401
-
